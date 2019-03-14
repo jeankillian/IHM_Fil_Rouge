8c72d09efbd19df3c56060656500d17430474767
dorianpelat <dorian.pelat@gmail.com>
2019-03-14 11:17:03 +0100

    First line of bottle interface

---
+++ bottletest.py
@@ -0,0 +1,21 @@
+from bottle import run , route, template
+
+@route('gameserver')
+def gameserver():
+    return '<h1>gameserver page<h1>'
+
+@route('lastgameresult')
+def last_game_result():
+    return '<h1>lastgameresult page<h1>'
+
+@route('configuration')
+def configuration():
+    return '<h1>configuration page<h1>'
+
+@route('statperday')
+def last_game_result():
+    return '<h1>statperday page<h1>'
+
+@route('statpergame')
+def last_game_result():
+    return '<h1>statpergame page<h1>'
