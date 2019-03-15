cf24d6beee602af16fbbcea75b293e5aceb9cf7d
jeankillian <jeankillian@yahoo.fr>
2019-03-15 10:58:24 +0100

    runnable program 'avec stats_per_day en plus'

--- test.py
+++ test.py
@@ -6,3 +6,10 @@ for s in ratatouille:
     liste.append(s)
 
 print(liste[0].nom)
+
+recupdata = m.StatsPerDay.liste_stat_per_marchine()
+print(recupdata)
+liste = []
+for obj in recupdata:
+    liste.append(obj)
+    print(obj.moyenne_partie)
